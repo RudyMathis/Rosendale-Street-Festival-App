d5200103f46d5044a640318f90a0704c25887f4b
RudyMathis <rudyjmathis@gmail.com>
2024-12-30 18:59:03 -0500

    fixing backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
+// import express from "express";
+// import cors from "cors";
+// import records from "./routes/record.js";
+// import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
+
+// const PORT = process.env.PORT || 5050;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/record", records);
+// app.use("/record", isAcceptedRouter);
+
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
+import isAcceptedRouter from "./routes/isAcceptedRouter.js";
 
-const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
@@ -11,7 +28,5 @@ app.use(express.json());
 app.use("/record", records);
 app.use("/record", isAcceptedRouter);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// Export the app for Vercel
+export default app;
