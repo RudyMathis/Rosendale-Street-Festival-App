9ef591c41ba644925510737f246185b4a56701fb
RudyMathis <rudyjmathis@gmail.com>
2024-12-31 13:01:01 -0500

    :(

--- server/server.js
+++ server/server.js
@@ -6,12 +6,7 @@ import isAcceptedRouter from "./routes/isAcceptedRouter.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://streetfestivalapp.vercel.app/"],
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 app.use("/record", isAcceptedRouter);
