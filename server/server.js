6e9e89524fa2edd51faf959853bb03d9781ab04c
RudyMathis <rudyjmathis@gmail.com>
2024-12-31 17:58:24 -0500

    .

--- server/server.js
+++ server/server.js
@@ -1,27 +1,17 @@
-// import express from "express";
-// import cors from "cors";
-// import records from "./routes/record.js";
-// import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
 
-// const PORT = process.env.PORT || 5050;
-// const app = express();
+const PORT = process.env.PORT || 5050;
+const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use("/record", records);
-// app.use("/record", isAcceptedRouter);
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+app.use("/record", isAcceptedRouter);
 
-// // start the Express server
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
-
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('server', { title: 'Express' });
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-module.exports = router;
