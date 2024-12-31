203d14bf674ebe8342819f83e6d696e37501c991
RudyMathis <rudyjmathis@gmail.com>
2024-12-31 11:07:04 -0500

    another attempted fix

--- server/server.js
+++ server/server.js
@@ -1,23 +1,45 @@
+// import express from "express";
+// import cors from "cors";
+// import records from "./routes/record.js";
+// import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
+
+// const PORT = process.env.PORT || 5050;
+// const app = express();
+
+// app.use(cors(
+//   {
+//     origin: "https://rosendale-street-festival-app.vercel.app",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true
+//   }
+// ));
+// app.use(express.json());
+// app.use("/record", records);
+// app.use("/record", isAcceptedRouter);
+
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-import isAcceptedRouter from "./routes/isAcceptedRouter.js"; 
+import isAcceptedRouter from "./routes/isAcceptedRouter.js";
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 5050;
+dotenv.config({ path: './config.env' });
+
 const app = express();
 
-app.use(cors(
-  {
-    origin: "https://rosendale-street-festival-app.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: "https://rosendale-street-festival-app.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use("/record", records);
 app.use("/record", isAcceptedRouter);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+export default app;
