93c7be81a950c3739bfa86e9018c40e875f3ea54
RudyMathis <rudyjmathis@gmail.com>
2025-02-02 20:55:23 -0500

    Added some fields
    Also updated record detail some with an edit button and link now links

--- client/src/UI/LabelDetail.tsx
+++ client/src/UI/LabelDetail.tsx
@@ -2,12 +2,20 @@ type LabelDetailProps = {
     label: string;
     value: React.ReactNode;
     style?: React.CSSProperties;
+    type?: string;
+    link?: string;
 };
 
-const LabelDetail = ({ label, value, style }: LabelDetailProps) => (
+const LabelDetail = ({ label, value, style, type, link }: LabelDetailProps) => (
     <div style={style}>
         <strong>{label}: </strong>
+        {type === 'link' 
+        ?   <a href={link} target="_blank" rel="noopener noreferrer">
+                <span>{value !== null && value !== undefined && value !== '' ? value : 'N/A'}</span>
+            </a>
+        :
         <span>{value !== null && value !== undefined && value !== '' ? value : 'N/A'}</span>
+        }
     </div>
 );
 
