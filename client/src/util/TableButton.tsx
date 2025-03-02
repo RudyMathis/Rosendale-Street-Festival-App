3c098eea213604a3b21cc0fd77e92f6d02b1aa05
RudyMathis <rudyjmathis@gmail.com>
2025-03-02 14:47:22 -0500

    mobile changes
    Some css mobile clean up specifically for iphone

--- client/src/util/TableButton.tsx
+++ client/src/util/TableButton.tsx
@@ -24,20 +24,22 @@ const TableButton = ({
 }: TableButtonProps) => (
     <th className={className}>
         <button onClick={onClick} className="table-header-button">
-            <span>{label}</span>
-            {sortConfig?.key === columnKey && (
-                <span className="sort-icon">
-                    {sortConfig.direction === "desc" ? (
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <polyline points="6 15 12 9 18 15"></polyline>
-                        </svg>
-                    ) : (
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <polyline points="6 9 12 15 18 9"></polyline>
-                        </svg>
-                    )}
-                </span>
-            )}
+            <div>{label}
+                {sortConfig?.key === columnKey && (
+                    <span className="sort-icon">
+                        {sortConfig.direction === "desc" ? (
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <polyline points="6 15 12 9 18 15"></polyline>
+                            </svg>
+                        ) : (
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <polyline points="6 9 12 15 18 9"></polyline>
+                            </svg>
+                        )}
+                    </span>
+                )}
+            </div>
+
         </button>
     </th>
 );
